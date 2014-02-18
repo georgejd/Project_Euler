f1643835ba218f1248aa2b8d5f26a401d63cd2ab
georgejd <george.jeremiah@gmail.com>
2014-02-18 13:35:21 -0500

    Update pe21.py

--- pe21.py
+++ pe21.py
@@ -13,6 +13,6 @@ for i in range(220, 10000):
 	for j in range(i, 10000):
 		if factors(i) == j and factors(j) == i and i != j:
 			answer = answer + i + j
-	text_file = open("PE21.txt", "w")
-	text_file.write("%s"%answer)
-	text_file.close()
+text_file = open("PE21.txt", "w")
+text_file.write("%s"%answer)
+text_file.close()
